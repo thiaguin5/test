68a8c6e822167e246e40b943972eb4d443830520
thiago amaral julio <amaralt778@gmail.com>
2025-12-07 01:16:40 -0300

    add script e condição

---
+++ site.js/script.js
@@ -0,0 +1,19 @@
+const lista = document.querySelector("lista");
+
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+
+});
+   
+
+//condição para retirar a li com lista 
+
+if (lista) {
+lista.remove();
+
+
+
+
+};
